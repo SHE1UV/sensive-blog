abdf312ec8ec6433fb8bca6ddbae2b708f468d84
SHE1UV <126342687+SHE1UV@users.noreply.github.com>
2024-07-22 20:43:31 +0200

    move queries into QuerySet methods

--- blog/views.py
+++ blog/views.py
@@ -24,30 +24,13 @@ def serialize_tag(tag):
     }
 
 
-def optimize_comment_count(most_popular_posts):
-    most_popular_posts_ids = [post.id for post in most_popular_posts]
-    posts_with_comments = (
-        Post.objects.filter(id__in=most_popular_posts_ids)
-        .annotate(comments_count=Count('comments', distinct=True))
-        .prefetch_related('author')
-    )
-    posts_comments_dict = dict(posts_with_comments.values_list('id', 'comments_count'))
-
-    for post in most_popular_posts:
-        post.comments_count = posts_comments_dict.get(post.id, 0)
-
-
 def index(request):
 
     most_popular_posts = (
-        Post.objects.annotate(
-            likes_count=Count('likes', distinct=True),
-        )
-        .order_by('-likes_count')
-        .prefetch_related('author')[:5]
+       Post.objects.popular()
+        .prefetch_related("author")[:5]
+        .fetch_with_comments_count()
     )
-
-    optimize_comment_count(most_popular_posts)
 
     fresh_posts = (
         Post.objects.annotate(comments_count=Count('comments'))
@@ -98,10 +81,9 @@ def post_detail(request, slug):
     most_popular_tags = Tag.objects.popular()[:5]
 
     most_popular_posts = (
-        Post.objects.annotate(
-            likes_count=Count('likes', distinct=True),
-            comments_count=Count('comments', distinct=True),
-        )
+        Post.objects.popular()
+        .prefetch_related("author")[:5]
+        .fetch_with_comments_count()
     )
 
     context = {
@@ -119,10 +101,12 @@ def tag_filter(request, tag_title):
 
     most_popular_tags = Tag.objects.popular()[:5]
 
-    most_popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by(
-        '-likes_count'
-    )[:5]
-
+    most_popular_posts = (
+        Post.objects.popular()
+        .prefetch_related("author")[:5]
+        .fetch_with_comments_count()
+    )
+    
     related_posts = tag.posts.annotate(comments_count=Count('comment', distinct=True))[
         :20
     ]
