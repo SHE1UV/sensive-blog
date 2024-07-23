60c7118e825b3e60dd35561ae0b1030ad8bd455d
SHE1UV <126342687+SHE1UV@users.noreply.github.com>
2024-07-23 19:41:12 +0200

    formatted the code

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ def serialize_post(post):
         'published_at': post.published_at,
         'slug': post.slug,
         'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
+        'first_tag_title': post.tags.all()[0].title if post.tags.exists() else None,
     }
 
 
@@ -23,47 +23,27 @@ def serialize_tag(tag):
         'posts_with_tag': tag.posts_count,
     }
 
-def index(request):
-    most_popular_posts = (
-        Post.objects.popular().prefetch_post_details().fetch_with_comments_count()[:5]
-    )
 
-    most_fresh_posts = (
-        Post.objects.annotate(comments_count=Count('comments'))
-        .prefetch_post_details()
-        .order_by('-published_at')
-    )[:5]
-
+def get_common_context():
+    most_popular_posts = Post.objects.popular().prefetch_post_details().fetch_with_comments_count()[:5]
     most_popular_tags = Tag.objects.fetch_posts_count().popular()[:5]
-
-    context = {
+    return {
         'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
+
+
+def index(request):
+    most_fresh_posts = Post.objects.annotate(comments_count=Count('comments')).prefetch_post_details().order_by('-published_at')[:5]
+    context = get_common_context()
+    context['page_posts'] = [serialize_post(post) for post in most_fresh_posts]
     return render(request, 'index.html', context)
 
 
 def post_detail(request, slug):
-    post = get_object_or_404(
-        Post.objects.annotate(
-            likes_count=Count('likes', distinct=True)
-        ).prefetch_post_details(),
-        slug=slug,
-    )
-
+    post = get_object_or_404(Post.objects.annotate(likes_count=Count('likes', distinct=True)).prefetch_post_details(), slug=slug)
     comments = post.comments.select_related('author')
-    serialized_comments = [
-        {
-            'text': comment.text,
-            'published_at': comment.published_at,
-            'author': comment.author.username,
-        }
-        for comment in comments
-    ]
-
-    related_tags = post.tags.all()
-
+    serialized_comments = [{'text': comment.text, 'published_at': comment.published_at, 'author': comment.author.username} for comment in comments]
     serialized_post = {
         'title': post.title,
         'text': post.text,
@@ -73,42 +53,21 @@ def post_detail(request, slug):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in related_tags],
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
     }
-
-    most_popular_tags = Tag.objects.fetch_posts_count().popular()[:5]
-
-    most_popular_posts = (
-        Post.objects.popular().prefetch_post_details().fetch_with_comments_count()[:5]
-    )
-
-    context = {
-        'post': serialized_post,
-        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
-        'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
-    }
+    context = get_common_context()
+    context['post'] = serialized_post
     return render(request, 'post-details.html', context)
 
 
 def tag_filter(request, tag_title):
     tag = get_object_or_404(Tag.objects.fetch_posts_count(), title=tag_title)
-
-    most_popular_tags = Tag.objects.fetch_posts_count().popular()[:5]
-
-    most_popular_posts = (
-        Post.objects.popular().prefetch_post_details().fetch_with_comments_count()[:5]
-    )
-
-    related_posts = tag.posts.annotate(
-        comments_count=Count('comments')
-    ).prefetch_post_details()[:20]
-
-    context = {
+    related_posts = tag.posts.annotate(comments_count=Count('comments')).prefetch_post_details()[:20]
+    context = get_common_context()
+    context.update({
         'tag': tag.title,
-        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
         'posts': [serialize_post(post) for post in related_posts],
-        'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
-    }
+    })
     return render(request, 'posts-list.html', context)
 
 
